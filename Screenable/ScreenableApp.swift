81bba62e3548eb9a41b3eaa800e18f61b23bc388
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-10 06:53:16 +0100

    Initial Commit

---
+++ Screenable/ScreenableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenableApp.swift
+//  Screenable
+//
+//  Created by Zaid Neurothrone on 2022-12-10.
+//
+
+import SwiftUI
+
+@main
+struct ScreenableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
